aa33e76b834de030cbb7a3fc89aacc8f518039f4
Sowmya <sowmyabs.ec@gmail.com>
2023-01-11 22:43:54 +0530

    updating

--- Assignments/Blog-article-assignment/blog_project/app/controllers/blog_controller.rb
+++ Assignments/Blog-article-assignment/blog_project/app/controllers/blog_controller.rb
@@ -1,2 +1,18 @@
 class BlogController < ApplicationController
+
+    def index
+        render json:Blog.all
+    end
+
+    def show
+        puts params[:id]
+        b = Blog.exists?(params[:id])
+        if b
+        v = Blog.find(params[:id])
+        render json:v
+        else
+        render html:"Data not found"     
+       end
+    end 
+
 end
